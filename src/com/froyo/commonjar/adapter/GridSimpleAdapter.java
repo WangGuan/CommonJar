bb0e4c3b85b27de6fb5dc4ec30658266ffb18af8
Rhino <793797714@qq.com>
2015-05-12 00:21:59 +0800

    新增GridSimpleAdapter类，将gridListVIew当着listView来处理，方便增加header和footer

---
+++ src/com/froyo/commonjar/adapter/GridSimpleAdapter.java
@@ -0,0 +1,93 @@
+package com.froyo.commonjar.adapter;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.froyo.commonjar.activity.BaseActivity;
+import com.froyo.commonjar.adapter.ListAsGridBaseAdapter;
+import com.froyo.commonjar.utils.RUtils;
+import com.froyo.commonjar.utils.Utils;
+
+public abstract class GridSimpleAdapter<E>  extends ListAsGridBaseAdapter { 
+
+	private List<E> data;
+
+	protected BaseActivity activity;
+
+	private int layoutId;
+
+	private Class<?> holderClass;
+
+	protected Object holder;
+	
+	private Class<?> cls;
+
+	public GridSimpleAdapter(List<E> data, BaseActivity activity, int layoutId,
+			Class<?> holderClass,Class<?> cls) {
+		super(activity);
+		this.data = data;
+		this.activity = activity;
+		this.layoutId = layoutId;
+		this.holderClass = holderClass;
+		this.cls=cls;
+		setNumColumns(setColumns());
+	}
+
+	@Override
+	public E getItem(int arg0) {
+		if (!Utils.isEmpty(data)) {
+			return data.get(arg0);
+		}
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public int getItemCount() {
+		if (!Utils.isEmpty(data)) {
+			return data.size();
+		}
+		return 0;
+	}
+
+	@Override
+	protected View getItemView(int position, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			convertView = activity.makeView(layoutId);
+			holder = buildHolder(convertView);
+			convertView.setTag(holder);
+		} else {
+			holder = convertView.getTag();
+		}
+		// 避免Item在滚动中出现黑色背景
+		convertView.setDrawingCacheEnabled(false);
+		E item = getItem(position);
+		doExtra(convertView, item,position);
+		return convertView;
+	}
+	
+	private Object buildHolder(View convertView) {
+		try {
+			holder = holderClass.newInstance();
+			for (Field f : holderClass.getDeclaredFields()) {
+				String name = f.getName();
+				f.setAccessible(true);
+				// ViewHolder的属性，不论类型都初始化赋值
+				f.set(holder, convertView.findViewById(RUtils.getId(name,cls)));
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("holder初始化出错    " + e);
+		}
+		return holder;
+	}
+
+	public abstract void doExtra(View convertView, E item,int position);
+	public abstract int setColumns();
+}
